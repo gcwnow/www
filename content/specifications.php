f48074664053b1ea55a63999020e8e9a78493f84
Jorik Duurkoop <surkow@gmail.com>
2013-01-15 18:07:17 +0100

    Updated specifications to match Kickstarter update.

--- content/specifications.php
+++ content/specifications.php
@@ -11,8 +11,8 @@ include "includes/header.php";
 <div class="table-row"><div class="row1 text_bold">GPU:</div><div class="row1">Vivante GC860, capable of OpenGL ES 2.0</div></div>
 <div class="table-row"><div class="row0 text_bold">Display:</div><div class="row0">3.5 inch LCD with 320x240 pixels; 4:3 aspect ratio is ideal for retro gaming</div></div>
 <div class="table-row"><div class="row1 text_bold">Operating System:</div><div class="row1">Linux 3.x (<a href="http://www.treewalker.org/opendingux/">OpenDingux</a>)</div></div>
-<div class="table-row"><div class="row0 text_bold">Memory:</div><div class="row0">256 MB DDR2</div></div>
-<div class="table-row"><div class="row1 text_bold">Internal storage:</div><div class="row1">8 GB, most of which is available for applications and data</div></div>
+<div class="table-row"><div class="row0 text_bold">Memory:</div><div class="row0">512 MB DDR2</div></div>
+<div class="table-row"><div class="row1 text_bold">Internal storage:</div><div class="row1">16 GB, most of which is available for applications and data</div></div>
 <div class="table-row"><div class="row0 text_bold">External storage:</div><div class="row0">micro SDHC up to 32 GB or micro SDXC of 64 GB (SDXC cards must be reformatted before use)</div></div>
 <div class="table-row"><div class="row1 text_bold">Connectivity:</div><div class="row1">Mini USB 2.0 OTG</div></div>
 <div class="table-row"><div class="row1 text_bold"></div><div class="row1">Mini HDMI 1.3 out</div></div>
