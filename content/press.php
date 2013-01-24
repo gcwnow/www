0e4c786514fed06fcac523528fae4d95b3f09782
Jorik Duurkoop <surkow@gmail.com>
2013-01-24 01:43:30 +0100

    Press released added to press page.

--- content/press.php
+++ content/press.php
@@ -12,6 +12,7 @@ include "includes/header.php";
 <div class="table-row"><div class="row0 text_bold"><a href="files/logos.zip">Logos</a></div><div class="row0">GCW Zero Logo. This includes variations of the logo for light and dark backgrounds, in SVG and PNG format.</div></div>
 <div class="table-row"><div class="row1 text_bold"><a href="files/photos.zip">Photos</a></div><div class="row1">Promotional photos of a black GCW Zero device.</div></div>
 <div class="table-row"><div class="row0 text_bold"><a href="files/banners.zip">Banners</a></div><div class="row0">Promotional banners for the GCW Zero. This includes a banner for both horizontal and vertical orientations.</div></div>
+<div class="table-row"><div class="row1 text_bold"><a href="files/gcw press release.pdf">Press Release</a></div><div class="row1">Official press release, released on Januari 17, 2013.</div></div>
 </div>
 
 </div>
