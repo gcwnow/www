54fd3dce547dccd2dd8e0dc4c1acbed201028d28
Jorik Duurkoop <surkow@gmail.com>
2013-01-28 03:54:30 +0100

    Added legal page, new game screenshots and preliminary work for new header and footer.

---
+++ content/legal.php
@@ -0,0 +1,24 @@
+<?php
+$page = "legal";
+include "includes/header.php";
+?>
+<div id="content">
+
+<h1>Legal</h1>
+
+<p>&copy Copyright Game Consoles Worldwide. Game Consoles Worldwide owns the right to the GCW Zero logo and the GCW Zero trade name.</p>
+
+<p>All trademarks displayed on the website are property of their respective owners. Licensing information of assets included in the website are displayed below.</p>
+
+<div class="table">
+<div class="table-row"><div class="row0 text_bold">GCW Zero Font</div><div class="row0">&copy Copyright Ivan Diaz.<br>Licensed under Attribution-ShareAlike CC BY-SA.</div></div>
+<div class="table-row"><div class="row1 text_bold">Promotional Photos GCW Zero</div><div class="row1">&copy Copyright Andreas Bjerkeholt.<br>Licensed under Attribution-ShareAlike CC BY-SA. </div></div>
+<div class="table-row"><div class="row0 text_bold">Twitter Logo</div><div class="row0">Implicit permission obtained according to the <a href="https://twitter.com/logo">Brand Permissions Center</a> of Facebook.</div></div>
+<div class="table-row"><div class="row1 text_bold">Facebook Logo</div><div class="row1">Implicit permission obtained according to the <a href="https://www.facebook.com/brandpermissions/logos.php">Trademark and Content Display Policy</a> of Twitter.</div></div>
+</div>
+
+</div>
+
+<?php
+include "includes/footer.php";
+?>
