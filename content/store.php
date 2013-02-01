cbe71609c4cf99fdd4365014598ce55e02b53b06
Jorik Duurkoop <surkow@gmail.com>
2013-02-01 16:25:37 +0100

    Updated store page.

--- content/store.php
+++ content/store.php
@@ -7,7 +7,11 @@ include "includes/header.php";
 
 <h1>Where To Buy</h1>
 
-<p>The Zero is ready to start full production. But we need your help. By ourselves we can only finance small batches, but by investing in our <a href="http://www.kickstarter.com/projects/gcw/gcw-zero-open-source-gaming-handheld">Kickstarter project</a> we can get a lot of devices produced in a short time, giving the Zero a flying start.</p>
+<p>With $238,498 pledged of the intial goal of $130,000 the <a href="http://www.kickstarter.com/projects/gcw/gcw-zero-open-source-gaming-handheld">Kickstarter campaign</a> has been a great success!</p>
+
+<a href="http://www.kicktraq.com/projects/gcw/gcw-zero-open-source-gaming-handheld/" target="_blank"><img src="http://www.kicktraq.com/projects/gcw/gcw-zero-open-source-gaming-handheld/minichart.png" alt="GCW-Zero: Open Source Gaming Handheld -- Kicktraq Mini" title="GCW-Zero: Open Source Gaming Handheld -- Kicktraq Mini"></a>
+
+<p>May 2013 is the estimated date when the GCW Zero will be available to the general public. The device will be sold at gcw-zero.com, with <a href="https://www.dragonbox.de">dragonbox.de</a> and <a href="http://ithic.com/">ithic.com</a> as official resellers (and others).</p>
 
 <a href="http://www.kickstarter.com/projects/gcw/gcw-zero-open-source-gaming-handheld"><img class="image_full" src="images/kickstarter-logo-light.png"></a>
 
