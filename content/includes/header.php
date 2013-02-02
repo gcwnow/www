89f1b0a64a0d54a75d048547d307a3d9abef8ea1
Jorik Duurkoop <surkow@gmail.com>
2013-02-03 00:02:51 +0100

    Header update

--- content/includes/header.php
+++ content/includes/header.php
@@ -21,7 +21,7 @@ if($page == "home") {
 onload = function() {
     var interval = 4000;
     var dir = "images/slideshow/";
-    var img = ["01.png", "02.png", "03.png", "08b.png", "04.png", "05.png", "03b.png", "09b.png", "06.png", "05b.png", "07.png", "08.png", "09.png", "11.png", "10.png"];
+    var img = ["01.png", "02.png", "12.png", "08b.png", "04.png", "05.png", "03b.png", "09b.png", "06.png", "05b.png", "07.png", "08.png", "09.png", "11.png", "10.png"];
     img = appendValue(dir, img);
     img.current = 0;
     autoSwitch(img, interval);
@@ -50,16 +50,15 @@ onload = function() {
 <div id="header">
 <div id="header_box">
 GCW Zero
-<div id="navigation">
-<ul>
+<ul id="navigation">
 <li><a href='/'><span class="gcwlogo1">GCW</span><span class="gcwlogo2">ZERO</span></a></li>
 <!--<li><a href='news'>News</a></li>-->
+
 <li><a href='specifications'>Specifications</a></li>
 <li><a href='media'>Media</a></li>
 <li><a href='store'>Where To Buy</a></li>
 <li><a href='develop'>Development</a></li>
 </ul>
-</div>
 
 </div>
 </div>
