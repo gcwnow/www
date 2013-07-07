0ab34c2623a141640067edb7863ec1dc2516d839
Maarten ter Huurne <maarten@treewalker.org>
2013-07-07 14:00:43 +0200

    Format update screenshots like image gallery

--- content/updates.php
+++ content/updates.php
@@ -28,27 +28,27 @@ First make sure you have a backup of all valuable user data you have on your GCW
 <p>
 Download the most recent updater application above. Transfer the application (OPK file) to the <code>apps/</code> directory on your GCW Zero. Then start the application from the menu:
 </p>
-<p>
-<img src="images/update/menu.png">
-<img src="images/update/notice.png">
-</p>
+<ul class="gallery">
+<li><img src="images/update/menu.png"></li>
+<li><img src="images/update/notice.png"></li>
+</ul>
 <p>
 You can select an option with the d-pad and confirm your choice with the <span style="font-variant: small-caps;">start</span> button.
 </p>
 <p>
 While the update is in progress, you'll see progress information being printed showing what the updater is doing. The update process takes a few minutes, please be patient while it does its work, even if you don't see anything happening for a while on the screen. The output should look like this:
 </p>
-<p>
-<img src="images/update/progress.png">
-<img src="images/update/done.png">
-</p>
+<ul class="gallery">
+<li><img src="images/update/progress.png"></li>
+<li><img src="images/update/done.png"></li>
+</ul>
 <p>
 When you press <span style="font-variant: small-caps;">start</span> again, the system will restart. After the restart you can check that the new kernel and rootfs were installed in the System Info application:
 </p>
-<p>
-<img src="images/update/menu-systeminfo.png">
-<img src="images/update/systeminfo.png">
-</p>
+<ul class="gallery">
+<li><img src="images/update/menu-systeminfo.png"></li>
+<li><img src="images/update/systeminfo.png"></li>
+</ul>
 <p>
 The compile date for the kernel and rootfs should be equal to or very close to the date of the updater application.
 </p>
