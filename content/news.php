f1104a1c2ff376b07f782bd5dc2fb3eb5f51d65f
Jorik Duurkoop <surkow@gmail.com>
2013-10-14 18:43:10 +0200

    Griffon Legend news article added.

--- content/news.php
+++ content/news.php
@@ -11,13 +11,17 @@ if(isset($_GET['id'])) {
     $product_id = $_GET['id'];
     if($product_id == 1) {
         include "articles/2013-09-13_milestone_etnaviv.php";
+    } elseif($product_id == 2) {
+        include "articles/2013-10-13_game_release_griffon_legend.php";
     } else {
         echo 'Page does not exist.';
     }
 
 } else {
     echo '<h1>News</h1>
+2013-10-13 - <a href="news.php?id=2">Game Release - Griffon Legend</a><br>
 2013-09-13 - <a href="news.php?id=1">Etnaviv Milestone</a>';
+
 }
 
 ?>
