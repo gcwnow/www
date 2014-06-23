dbfbf7ae6f5924d82230c9b0ed5a575514f94974
Surkow <surkow@gmail.com>
2014-06-23 16:49:13 +0200

    Add flash video fallback

--- content/articles/2014-06-22_game_release_the_sqrxz_series.php
+++ content/articles/2014-06-22_game_release_the_sqrxz_series.php
@@ -1,18 +1,17 @@
 <h1>News :: Game release - The Sqrxz Series</h1>
-Date: 2014-06-22, Authors: Albert H. & Surkow
+Date: 2014-06-22, Authors: Albert H. &amp; Surkow
 
 <h2>Two seconds until death!</h2>
 
 <p>Finally, with much effort put in by our very own porting wizard Zear, comes Sqrxz, a four-part game series created by the members of team Retroguru<a href=#1>[1]</a> based on the original DOS classic Sqrxz. With the addition of a remake of the first game, the Sqrxz series is finally available on the GCW Zero in full, and able to be enjoyed in all its migraine-inducing glory. Hop along the trail against terrifying odds, as you, eponymous lapine-ish hero, will be poised to save the day. Do you have spastic fingers not quite on the verge of carpal tunnel syndrome? Let Sqrxz remedy that. When you start dying, you will find yourself showing no mercy as you crush every unfortunate enemy underfoot!</p>
 
-<object width="650" height="400" class="center_container">
+<object width="650" height="400" class="center_container"
+        data="https://www.youtube.com/v/n_ZB7bd3xTI"
+        type="application/x-shockwave-flash">
   <param name="movie"
          value="https://www.youtube.com/v/n_ZB7bd3xTI"></param>
   <param name="allowScriptAccess" value="always"></param>
-  <embed src="https://www.youtube.com/v/n_ZB7bd3xTI"
-         type="application/x-shockwave-flash"
-         allowscriptaccess="always"
-         width="650" height="400"></embed>
+  <a class="image_center_a" href="https://www.youtube.com/watch?v=n_ZB7bd3xTI" title="Watch video on YouTube" style="background-image: url('http://img.youtube.com/vi/n_ZB7bd3xTI/0.jpg');"></a>
 </object>
 
 <h2>Re-release of the original Sqrxz</h2>
@@ -21,14 +20,13 @@ Date: 2014-06-22, Authors: Albert H. & Surkow
 
 <p>For the 2010 remake of the original DOS classic, a new mode with "deluxe" graphics has been introduced. The main game has been modified, so that now you must collect all items to open the door at the end of each level, with an additional three levels unique to the remake. A port of the remake has been released for the GCW Zero and can be found in the official GCW Zero repository<a href=#2>[2]</a>.</p>
 
-<object width="650" height="400" class="center_container">
+<object width="650" height="400" class="center_container"
+        data="https://www.youtube.com/v/IygLqq1U2BU&amp;start=38"
+        type="application/x-shockwave-flash">
   <param name="movie"
-         value="https://www.youtube.com/v/IygLqq1U2BU&start=38"></param>
+         value="https://www.youtube.com/v/IygLqq1U2BU&amp;start=38"></param>
   <param name="allowScriptAccess" value="always"></param>
-  <embed src="https://www.youtube.com/v/IygLqq1U2BU&start=38"
-         type="application/x-shockwave-flash"
-         allowscriptaccess="always"
-         width="650" height="400"></embed>
+  <a class="image_center_a" href="https://www.youtube.com/watch?v=IygLqq1U2BU&amp;t=38" title="Watch video on YouTube" style="background-image: url('http://img.youtube.com/vi/IygLqq1U2BU/0.jpg');"></a>
 </object>
 
 <h2>Gameplay</h2>
