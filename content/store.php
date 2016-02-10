6d74f592ebf75573890cbd435b7093fe3b04f2bb
Surkow <surkow@gmail.com>
2016-02-10 01:54:17 +0100

    Updated store page, removed non active resellers

--- content/store.php
+++ content/store.php
@@ -12,16 +12,9 @@ include "includes/header.php";
 <ul class="gallery resellers">
 
 <li>
-<a href="http://www.thinkgeek.com/product/168f/">
-<span class="bgimage" style="background-image: url('images/resellers/ThinkGeek Logo 2014.png');"></span>
-<span class="label">ThinkGeek (USA)</span>
-</a>
-</li>
-
-<li>
-<a href="http://geekswithwives.com/buy-gcw-zero/">
-<span class="bgimage" style="background-image: url(images/resellers/GeeksFinal-2_400x400.jpg); background-size: 100px 100px;"></span>
-<span class="label">Geeks With Wives (USA)</span>
+<a href="http://thegww.com/buy-gcw-zero/">
+<span class="bgimage" style="background-image: url(images/resellers/gww_logo_small.jpg)"></span>
+<span class="label">Geeks WorldWide (USA & Canada)</span>
 </a>
 </li>
 
@@ -33,20 +26,6 @@ include "includes/header.php";
 </li>
 
 <li>
-<a href="https://www.dragonbox.de/en/84-gcw-zero/">
-<span class="bgimage" style="background-image: url(images/resellers/dragonbox.jpg);"></span>
-<span class="label">DragonBox (Europe)</span>
-</a>
-</li>
-
-<li>
-<a href="http://www.gameseek.co.uk/stores/GCW/">
-<span class="bgimage" style="background-image: url(images/resellers/gameseek_logo_small.png);"></span>
-<span class="label">GameSeek (United Kingdom)</span>
-</a>
-</li>
-
-<li>
 <a href="http://www.extremehardware.co.uk/GCW_Zero_Open_Source_Handheld_Gaming_Console/p3267181_14213717.aspx">
 <span class="bgimage" style="background-image: url(images/resellers/extremehardware_logo_small.png);"></span>
 <span class="label">Extreme Hardware (United Kingdom)</span>
@@ -54,9 +33,9 @@ include "includes/header.php";
 </li>
 
 <li>
-<a href="http://otakutoyz.blogspot.com/2014/06/purewicked.html">
-<span class="bgimage" style="background-image: url(images/resellers/purewickedau.png);"></span>
-<span class="label">PureWicked.AU (Australia)</span>
+<a href="https://www.dragonbox.de/en/84-gcw-zero/">
+<span class="bgimage" style="background-image: url(images/resellers/dragonbox.jpg);"></span>
+<span class="label">DragonBox (Europe)</span>
 </a>
 </li>
 
