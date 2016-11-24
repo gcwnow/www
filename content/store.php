5cf066dc77c3fff4ec30356a6de976cb822294b1
Surkow <surkow@gmail.com>
2016-11-24 22:58:06 +0100

    Removal of old resellers

--- content/store.php
+++ content/store.php
@@ -12,23 +12,9 @@ include "includes/header.php";
 <ul class="gallery resellers">
 
 <li>
-<a href="http://thegww.com/buy-gcw-zero/">
-<span class="bgimage" style="background-image: url(images/resellers/gww_logo_small.jpg)"></span>
-<span class="label">Geeks WorldWide (USA & Canada)</span>
-</a>
-</li>
-
-<li>
 <a href="http://ithic.com/newsite/index.php?id_category=8&controller=category">
 <span class="bgimage" style="background-image: url(images/resellers/ithic.png);"></span>
 <span class="label">Ithic.com (USA)</span>
-</a>
-</li>
-
-<li>
-<a href="http://www.extremehardware.co.uk/GCW_Zero_Open_Source_Handheld_Gaming_Console/p3267181_14213717.aspx">
-<span class="bgimage" style="background-image: url(images/resellers/extremehardware_logo_small.png);"></span>
-<span class="label">Extreme Hardware (United Kingdom)</span>
 </a>
 </li>
 
